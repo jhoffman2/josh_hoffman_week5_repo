5680dae48de8c41d8553576f31e08582d8d1df17
Josh Hoffman <jh276903@scc-mmlab-01.local>
2019-02-19 15:05:01 -0500

    Added Whole Project

---
+++ 5401_Week5_Hoffman/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, 5))
+        {
+            if (hit.collider.tag == "Obstacle")
+            {
+                Debug.Log(hit.transform.gameObject + " is detected!");
+            }
+            
+        }
+        else
+        {
+            Debug.Log("Nothing ahead!");
+        }
+    }
+}
---
+++ 5401_Week5_Hoffman/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMotor : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+       
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 200))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+
+
+    }
+}
